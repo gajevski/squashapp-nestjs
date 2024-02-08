e3f62a9503fef5562f91f177181b7dd037aa1976
gajevski <mikolajxgajewski@gmail.com>
2024-02-08 19:10:22 +0100

    Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TerminusModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
