d277955e4725f6deed44f13741c49f376bdd43ab
gajevski <mikolajxgajewski@gmail.com>
2024-02-09 19:55:46 +0100

    Add login endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
+import { User } from './users/users.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -11,5 +13,11 @@ export class AppController {
     return this.health.check([
       async () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
     ]);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/v1/auth/login')
+  async login(@Request() req): Promise<User> {
+    return req.user
   }
 }
