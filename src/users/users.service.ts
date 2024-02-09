141671ffb4a15e39e04cdcfff383f33026cf11d7
gajevski <mikolajxgajewski@gmail.com>
2024-02-09 19:21:11 +0100

    Add sample users list

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+    userId: number;
+    username: string;
+    password: string;
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users: User[] = [
+        {
+            userId: 1,
+            username: "First",
+            password: "first"
+        },
+        {
+            userId: 2,
+            username: "Second",
+            password: "second"
+        }
+    ]
+
+}
