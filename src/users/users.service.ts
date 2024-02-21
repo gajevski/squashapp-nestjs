ec1484e5c87b9b399e8aa18573cb8449b9008d9d
gajevski <mikolajxgajewski@gmail.com>
2024-02-21 18:57:31 +0100

    Refactor users object

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,36 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/models/user';
 
+export const users: User[] = [
+    {
+        userId: 1,
+        username: "first",
+        password: "first",
+        image: "https://avatars.githubusercontent.com/u/29663156?v=4",
+        racket: {
+            name: "Wilson Hyper Hammer 120",
+            image: "https://www.squashtime.pl/images/thumbs/640_720/WRT967700_wilson_01.jpg",
+            purchaseDate: "October 2023",
+            totalMatchesPlayed: 26,
+            grip: "Toalson Ultra Grip 3Pack Black",
+            string: "Default"
+        },
+        statistics: {
+            matchesPlayed: 10,
+            matchesWon: 6,
+            matchesLost: 4,
+            winratio: 60,
+        },
+        activities: [
+            { id: 1, activityName: "Squash Match", date: "2023-10-01" },
+            { id: 2, activityName: "Training Session", date: "2023-10-02" }
+        ]
+    },
+]
+
 @Injectable()
 export class UsersService {
-    private readonly users: User[] = [
-        {
-            userId: 1,
-            username: "first",
-            password: "first",
-            image: "https://avatars.githubusercontent.com/u/29663156?v=4",
-            racket: {
-                name: "Wilson Hyper Hammer 120",
-                image: "https://www.squashtime.pl/images/thumbs/640_720/WRT967700_wilson_01.jpg",
-                purchaseDate: "October 2023",
-                totalMatchesPlayed: 26,
-                grip: "Toalson Ultra Grip 3Pack Black",
-                string: "Default"
-            },
-            statistics: {
-                matchesPlayed: 10,
-                matchesWon: 6,
-                matchesLost: 4,
-                winratio: 60,
-            },
-            activities: [
-                { id: 1, activityName: "Squash Match", date: "2023-10-01" },
-                { id: 2, activityName: "Training Session", date: "2023-10-02" }
-            ]
-        },
-    ]
 
     async findUser(username: string): Promise<User | undefined> {
-        return this.users.find((user: User) => user.username === username);
+        return users.find((user: User) => user.username === username);
+    }
+
+    async createUser(user: User): Promise<User> {
+        users.push(user);
+        return user;
     }
 }
