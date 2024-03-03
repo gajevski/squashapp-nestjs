ca16a41411ceb65520c1a1cd1353396bf00c732d
gajevski <mikolajxgajewski@gmail.com>
2024-03-03 19:35:11 +0100

    Add progress mock

--- src/basic-tutorial/basic-tutorial.service.ts
+++ src/basic-tutorial/basic-tutorial.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+export const progress = [
+    {
+        userId: 1,
+        isServeFinished: false,
+        isRallyFinished: false,
+        isBalloutFinished: false,
+        isScoringFinished: false
+    }
+]
+
 @Injectable()
 export class BasicTutorialService {}
