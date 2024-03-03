8149e73411268d438bf1c8bb49250ce0115d3f28
gajevski <mikolajxgajewski@gmail.com>
2024-03-03 20:10:26 +0100

    Add method to update progress

--- src/basic-tutorial/basic-tutorial.service.ts
+++ src/basic-tutorial/basic-tutorial.service.ts
@@ -12,4 +12,22 @@ export const progress: BasicTutorial[] = [
 ]
 
 @Injectable()
-export class BasicTutorialService {}
+export class BasicTutorialService {
+
+    async updateBasicTutorialProgress(basicTutorialRequest: BasicTutorial): Promise<BasicTutorial | null> {
+        const progressToUpdateIndex: number = progress.findIndex((basicTutorial: BasicTutorial) => basicTutorial.userId === basicTutorialRequest.userId);
+        
+        if (progressToUpdateIndex !== -1) {
+          const currentProgress: BasicTutorial = progress[progressToUpdateIndex];
+          const updatedProgress = {
+            ...currentProgress,
+            ...basicTutorialRequest,
+          };
+          progress[progressToUpdateIndex] = updatedProgress;
+          return updatedProgress;
+        } else {
+          console.error(`User with ID ${basicTutorialRequest.userId} not found.`);
+          return null;
+        }
+      }
+}
