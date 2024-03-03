aca3af178d0b6466cdd57ec0bd259d5e8859abdd
gajevski <mikolajxgajewski@gmail.com>
2024-03-03 20:09:26 +0100

    Add export to basic tutorial module

--- src/basic-tutorial/basic-tutorial.module.ts
+++ src/basic-tutorial/basic-tutorial.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BasicTutorialService } from './basic-tutorial.service';
 
 @Module({
-  providers: [BasicTutorialService]
+  providers: [BasicTutorialService],
+  exports: [BasicTutorialService]
 })
 export class BasicTutorialModule {}
