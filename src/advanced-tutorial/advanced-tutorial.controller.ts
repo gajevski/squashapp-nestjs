8acebed2a6cb86e9aa99e92a2a43ad800d7258aa
gajevski <mikolajxgajewski@gmail.com>
2024-03-25 20:51:00 +0100

    Add advanced-tutorial controller

--- src/advanced-tutorial/advanced-tutorial.controller.ts
+++ src/advanced-tutorial/advanced-tutorial.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards, Request } from '@nestjs/common';
+import { AdvancedTutorialService, advancedTutorialProgress } from './advanced-tutorial.service';
+import { JwtGuard } from 'src/auth/jwt.auth.guard';
+import { AdvancedTutorial } from 'src/models/advanced-tutorial';
+import { UpdateAdvancedTutorialDto } from 'src/models/update-advanced-tutorial';
 
 @Controller('v1/advanced-tutorial/progress')
-export class AdvancedTutorialController {}
+export class AdvancedTutorialController {
+    constructor(private _advancedTutorialService: AdvancedTutorialService) { }
+
+    @UseGuards(JwtGuard)
+    @Put('/v1/advanced-tutorial/progress')
+    async updateAdvancedTutorialProgress(@Request() req, @Body() advancedTutorialRequest: UpdateAdvancedTutorialDto) {
+        return this._advancedTutorialService.updateAdvancedTutorialProgress(req.user.userId, advancedTutorialRequest);
+    }
+
+    @UseGuards(JwtGuard)
+    @Get('/v1/advanced-tutorial/progress')
+    async getAdvancedTutorialProgress(@Request() req) {
+        return advancedTutorialProgress.find((advancedTutorial: AdvancedTutorial) => advancedTutorial.userId === req.user.userId);
+    }
+}
