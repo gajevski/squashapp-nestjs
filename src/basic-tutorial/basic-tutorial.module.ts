daf3a2a4bba3e1cef0068e65dc5989c870ab7873
gajevski <mikolajxgajewski@gmail.com>
2024-03-25 20:51:51 +0100

    Create basic-tutorial controller

--- src/basic-tutorial/basic-tutorial.module.ts
+++ src/basic-tutorial/basic-tutorial.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BasicTutorialService } from './basic-tutorial.service';
+import { BasicTutorialController } from './basic-tutorial.controller';
 
 @Module({
   providers: [BasicTutorialService],
-  exports: [BasicTutorialService]
+  exports: [BasicTutorialService],
+  controllers: [BasicTutorialController]
 })
 export class BasicTutorialModule {}
