53ba914d58b282e196ad3ce1b9c6df7b09861f7d
gajevski <mikolajxgajewski@gmail.com>
2024-03-26 19:48:17 +0100

    Refactor health check method

--- src/status/status.controller.ts
+++ src/status/status.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
 
 @Controller('status')
-export class StatusController {}
+export class StatusController {
+    constructor(private _healthCheckService: HealthCheckService, private _http: HttpHealthIndicator) { }
+
+    @Get('v1/status')
+    @HealthCheck()
+    statusCheck() {
+        return this._healthCheckService.check([
+            async () => this._http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+        ]);
+    }
+}
