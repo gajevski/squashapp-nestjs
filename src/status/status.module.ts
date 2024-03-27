279a32ccc379b8dbac59998327d2c6ad59abbff6
gajevski <mikolajxgajewski@gmail.com>
2024-03-27 20:26:56 +0100

    Fix controllers

--- src/status/status.module.ts
+++ src/status/status.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StatusController } from './status.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
+  imports: [TerminusModule],
   controllers: [StatusController]
 })
 export class StatusModule {}
