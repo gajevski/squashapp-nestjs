426e95b045669cd0eac314230af47ae3bfe3a003
gajevski <mikolajxgajewski@gmail.com>
2024-03-27 20:26:54 +0100

    Fix controllers

--- src/basic-tutorial/basic-tutorial.controller.ts
+++ src/basic-tutorial/basic-tutorial.controller.ts
@@ -4,18 +4,18 @@ import { JwtGuard } from 'src/auth/jwt.auth.guard';
 import { BasicTutorial } from 'src/models/basic-tutorial';
 import { UpdateBasicTutorialDto } from 'src/models/update-basic-tutorial';
 
-@Controller('basic-tutorial')
+@Controller('v1/basic-tutorial')
 export class BasicTutorialController {
     constructor(private _basicTutorialService: BasicTutorialService) { }
 
     @UseGuards(JwtGuard)
-    @Put('/v1/basic-tutorial/progress')
+    @Put('/progress')
     async updateBasicTutorialProgress(@Request() req, @Body() basicTutorialRequest: UpdateBasicTutorialDto) {
         return this._basicTutorialService.updateBasicTutorialProgress(req.user.userId, basicTutorialRequest);
     }
 
     @UseGuards(JwtGuard)
-    @Get('/v1/basic-tutorial/progress')
+    @Get('/progress')
     async getBasicTutorialProgress(@Request() req) {
         return basicTutorialProgress.find((basicTutorial: BasicTutorial) => basicTutorial.userId === req.user.userId);
     }
